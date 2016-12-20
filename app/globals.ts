e3f702f92feafc060f6a9d6671e12cd1f8e3ba27
awemulya <awemulya@gmail.com>
2016-12-20 21:55:02 +0545

    kobocat  url

--- app/globals.ts
+++ app/globals.ts
@@ -2,6 +2,6 @@
 
 export var ng: string = "/static/ng";  
 export var version: string = "0.0.1";  
-export var KOBOCAT_URL: string = "http://192.168.1.2:8001";
-export var KPI_URL: string = "http://192.168.1.2:8000";
+export var KOBOCAT_URL: string = "http://kc.naxa.com.np/";
+export var KPI_URL: string = "http://bcss.naxa.com.np/";
 export var username: string = "amulya"; // set from login.component.ts
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
 var INTERPOLATION_REGEXP = /\[\[([\s\S]*?)\]\]/g; // default
 platformBrowserDynamic().bootstrapModule(AppModule, [
     {
